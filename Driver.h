61063427da6654926ecd0c868bb7c0558fd4efec
Chantell Akuffo <chantellakuffo@gmail.com>
2018-11-28 14:47:32 -0500

    added functions to driver class

--- Driver.h
+++ Driver.h
@@ -14,23 +14,68 @@ class Driver{
     void login(string Drivername);
     void logout();
     void CreateOrder(Time myTime, string TheInfo);
+    void depart() throw(logic_error);
+    void deliver() throw(logic_error);
+    void arrive() throw(logic_error);
     private:
     //list<Order> myOrder;
     Order myOrder[13];
     string myDriver;
+    int orderStatus;
+    //shows the status of the order
+    //0: order is at the restaurant
+    //1: driver is out for delivery
+    //2: driver has delivered order 
+    //3: driver is back at the restaurant
 
 };
 
-Driver::Driver(){
+Driver::Driver()
+{
 myDriver = "";
 }
 
-void Driver::login(string DriverName){
+void Driver::login(string DriverName)
+{
 myDriver= DriverName;
 }
 
-void Driver::logout(){
+void Driver::logout()
+{
 
+}
+
+void Driver:: depart() throw(logic_error)
+/*
+*pre-condition: order is at the restaurant
+*post-condition: driver is out for delivery
+ */
+{
+    if(orderStatus != 0)
+        throw logic_error("Order has not been received at the restaurant");
+    orderStatus = 1;
+}
+
+void Driver:: deliver() throw(logic_error)
+/*
+ *pre-condition: driver is out for delivery
+ *post-condition: driver has delivered order
+ */
+{
+    if(orderStatus != 1)
+        throw logic_error("Driver is not out to deliver order");
+    orderStatus = 2;
+}
+
+void Driver:: arrive() throw(logic_error)
+/*
+ *pre-condition: driver has delivered order
+ *post-condition: driver is back at the shop
+ */
+{
+    if(orderStatus != 2)
+        throw logic_error("Driver has not delivered order");
+    orderStatus = 3;
 }
 
 void Driver::CreateOrder(Time myTime, string TheInfo){
