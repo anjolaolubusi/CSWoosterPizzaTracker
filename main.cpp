50fb55f361c0b8805ebcf23888b7ea22b7e52ff3
chantella <45299613+chantella@users.noreply.github.com>
2018-12-05 14:33:01 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -113,34 +113,6 @@ int main()
 }*/
 }
 
-//Command line function:
-/**
-The idea is you would input a string and the compiler would scan the string for a command it recongize
-**/
-int Commander(Driver myDriver){
-    string cmd;
-    cout << " Command: ";
-    getline(cin, cmd);
-    int blackspace = cmd.find_first_of(" ");
-
-    if(cmd == "Quit" || cmd == "quit"){
-        return 0; //Exits cleanly
-    }else if(cmd == "Time" || cmd=="time"){
-
-        Commander(myDriver); //Returns to command line
-    }else if(cmd.substr(0, blackspace) == "order "){
-        blackspace = cmd.find_first_of(":"); //
-        Time timeMan;
-        timeMan.setTime(stoi(cmd.substr(blackspace-3, blackspace-1)), stoi(cmd.substr(blackspace+1, blackspace+3)));
-        myDriver.CreateOrder(timeMan," Test");
-        Commander(myDriver);
-    } else if(cmd == "print order"){
-    Commander(myDriver);
-    }else{
-    Commander(myDriver);
-    }
-}
-
 void login(string driver, Restaurant& arestaurant) throw (logic_error) 
 {
     Driver* DriverPtr =  arestaurant.getDriver(driver);
