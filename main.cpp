57101a7d3aea2618d2f4fe26cb0426794804d489
chantella <45299613+chantella@users.noreply.github.com>
2018-12-05 11:50:47 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,133 +1,207 @@
 #include <iostream>
-#include <string>
-#include <algorithm>
 #include "Driver.h"
-#include "Status.h"
-#include "Summary.h"
-using namespace std;
 
-int CommandInput(Driver myDriver);
-void commands_list();
+using namespace std;
+void login(string driver, Restaurant& aRestaurant) throw (logic_error);
+
+void logout(string driver, Restaurant& aRestaurant) throw (logic_error);
+
+void depart(const Time& time, const string driver, Restaurant& aRestaurant);
+
+void deliver(const Time& time, const string driver, const float tip, Restaurant& arestaurant);
+
+void arrive(const Time& time, const string driver, Restaurant& aRestaurant);
+
+int Commander(Driver myDriver);
+
+void print_help();
+
 
 int main()
 {
-    Driver myDriver;//This creates a default driver named myDriver
-    /**
-     Pre Conditions: None
-     Post: None
+    Restaurant Dominios;
+    
+    string command;
+    
+    string driver; //a string to hold the driver's name
+    
+    string string_time; //a string to hold time
+    
+    string info; //a string to hold an order's info
+    
+    Driver myDriver;
 
-     This method prints out all of the commands available to the user
-     It then asks the user if they would like an example of how to enter a command
-    */
-    commands_list();
-    /**
-      pre: A string has been entered into the terminal
-      post: It calls the method associated to the command entered.
-
-      This method asks the user to enter the command they would lie to use than
-      makes a call to the associated method
-    */
-    CommandInput(myDriver);
-}
-
-//Hello there
-
-void commands_list(){
-    cout << "Here is the list of commands you can enter:\nLogin <DriverName>"
-            "\nLogout <DriverName>\nOrder <Time> <Order Info>\nDepart <Time> <DriverName>"
-            "\nDeliver <Time> <DriverName> <tip>\nServe <Time>\nArrive <Time> <DriverName>"
-            "\nStatus\nSummary\nHelp\nQuit\n" << endl;
-    cout << "Would you like to see examples? Yes or No: ";
-
-    string cmd;
-    getline(cin, cmd);
-    transform(cmd.begin(), cmd.end(), cmd.begin(), ::tolower);
-    if (cmd == "yes"){
-        cout << "Here is an example of the ORDER function\n"
-                "ORDER TIME QUANTITY TYPE ADDRESS\n"
-                "order 9:55 2 veggie to 100 Liberty\n"
-                "----------------------------------\n"
-                "order 0:02 1 cheese 1 breadsticks "
-                "to 123 Holden Hall\n" << endl;
+    Commander(myDriver);
+    
+    print_help();
+    cin >> command;
+  
+   /*
+    while (command != "quit") {
+        
+        //Handles input and calls the corresponding function
+        if (command == "help") {
+            print_help();
+        }
+        else if (command == "login")
+        {
+            cin >> driver;
+            login(driver, Dominios);
+        }
+        
+        else if (command == "logout")
+        {
+            cin >> driver;
+            logout(driver, Dominios);
+        }
+        
+        else if (command == "order")
+        {
+            cin >> string_time;
+            getline(cin, info);
+            Time time(string_time);
+            order(time, info, Dominios);
+        }
+        
+        else if (command == "serve")
+        {
+            cin >> string_time;
+            Time time(string_time);
+            serve(time, Dominios);
+        }
+        
+        else if (command == "depart")
+        {
+            cin >> string_time;
+            Time time(string_time);
+            cin >> driver;
+            depart(time, driver, Dominios);
+        }
+        
+        else if (command == "deliver")
+        {
+            cin >> string_time;
+            Time time(string_time);
+            cin >> driver;
+            float tip;
+            cin >> tip;
+            deliver(time, driver, tip, Dominios);
+        }
+        
+        else if (command == "arrive")
+        {
+            cin >> string_time;
+            Time time(string_time);
+            cin >> driver;
+            arrive(time, driver, Dominios);
+        }
+        
+        else if (command == "status")
+            status(Dominios);
+        
+        else if (command == "summary")
+            summary(Dominios);
+        
+        else
+            cout << "Not a valid command" << endl;
+        
+        cin >> command;
     }
+    
+    return 0;
+}*/
 }
 
 //Command line function:
 /**
-      pre: A string has been entered into the terminal
-      post: It calls the method associated to the command entered.
-
-      This method asks the user to enter the command they would lie to use than
-      makes a call to the associated method
-*/
-int CommandInput(Driver myDriver){
+The idea is you would input a string and the compiler would scan the string for a command it recongize
+**/
+int Commander(Driver myDriver){
     string cmd;
-    Time timeMan;
     cout << " Command: ";
     getline(cin, cmd);
-    transform(cmd.begin(), cmd.end(), cmd.begin(), ::tolower);
     int blackspace = cmd.find_first_of(" ");
 
-    if (cmd.substr(0,blackspace) == "login"){
-
-        myDriver.login(cmd.substr(blackspace));
-        CommandInput(myDriver);
-    }
-    else if (cmd.substr(0,blackspace) == "logout"){
-
-    }
-    else if(cmd.substr(0, blackspace) == "order"){
-        if(myDriver.GetCurrentDrivers() != ""){
-        blackspace = cmd.find_first_of(":");
-        int hour = stoi(cmd.substr(blackspace-3, blackspace-1));
-        int minute = stoi(cmd.substr(blackspace+1, blackspace+3));
-        myDriver.CreateOrder(hour, minute, cmd.substr(blackspace+4));
-        CommandInput(myDriver);
-        } else {
-        cout << "Please log in" << endl;
-        }
-    }
-    else if (cmd.substr(0,blackspace) == "depart" && myDriver.GetCurrentDrivers() != "") {
-    if(myDriver.GetCurrentDrivers() != ""){
-        blackspace = cmd.find_first_of(":");
-        int hour = stoi(cmd.substr(blackspace-3, blackspace-1));
-        int minute = stoi(cmd.substr(blackspace+1, blackspace+3));
-        myDriver.depart(hour, minute, cmd.substr(blackspace+4));
-        CommandInput(myDriver);
-         } else {
-        cout << "Please log in" << endl;
-        CommandInput(myDriver);
-        }
-    }
-    else if (cmd.substr(0,blackspace) == "deliver") {
-
-    }
-    else if (cmd.substr(0,blackspace) == "serve") {
-
-    }
-    else if (cmd.substr(0,blackspace) == "arrive") {
-
-    }
-    else if (cmd.substr(0,blackspace) == "status") {
-        if(myDriver.GetCurrentDrivers() != ""){
-        Status currentStatus;
-        currentStatus.PrintStatus(myDriver);
-        CommandInput(myDriver);
-         } else {
-        cout << "Please log in" << endl;
-        CommandInput(myDriver);
-        }
-    }
-    else if (cmd.substr(0,blackspace) == "summary") {
-
-    }
-    else if(cmd == "help"){
-        commands_list();
-    }
-    else if(cmd == "quit"){
+    if(cmd == "Quit" || cmd == "quit"){
         return 0; //Exits cleanly
+    }else if(cmd == "Time" || cmd=="time"){
+
+        Commander(myDriver); //Returns to command line
+    }else if(cmd.substr(0, blackspace) == "order "){
+        blackspace = cmd.find_first_of(":"); //
+        Time timeMan;
+        timeMan.setTime(stoi(cmd.substr(blackspace-3, blackspace-1)), stoi(cmd.substr(blackspace+1, blackspace+3)));
+        myDriver.CreateOrder(timeMan," Test");
+        Commander(myDriver);
+    } else if(cmd == "print order"){
+    Commander(myDriver);
+    }else{
+    Commander(myDriver);
     }
-    else{
-        CommandInput(myDriver);
+}
+
+void login(string driver, Restaurant& arestaurant) throw (logic_error) {
+    Driver* DriverPtr =  arestaurant.getDriver(driver);
+    if (DriverPtr == nullptr) {
+        
+        Driver* aDriver = new Driver(driver);
+        arestaurant.addDriver(aDriver);
     }
+    else
+        throw logic_error("The driver is already logged in");
+}
+
+
+void logout(string driver, Restaurant& arestaurant) throw (logic_error){
+    
+    Driver* DriverPtr =  arestaurant.getDriver(driver);
+    
+    if (DriverPtr == nullptr) {
+        throw logic_error("Driver does not exist");
+    }
+    (*DriverPtr).logout();
+}
+void depart(const Time& time, const string driver, Restaurant& arestaurant) {
+    Order*departOrder = arestaurant.departNextOrder();
+    Driver*DriverPtr =  arestaurant.getDriver(driver);
+    DriverPtr->depart(time, *departOrder);
+}
+
+void deliver(const Time& time, const string driver, const float tip, Restaurant& arestaurant) {
+    Driver*DriverPtr =  arestaurant.getDriver(driver);
+    arestaurant.deliver(DriverPtr, time, tip);
+}
+
+void arrive(const Time& time, const string driver, Restaurant& arestaurant) {
+    Driver*DriverPtr =  arestaurant.getDriver(driver);
+    DriverPtr->arrive(time);
+    
+   // pre-condition: None
+// post-condition: Print help.
+void print_help()
+{
+    cout << endl << "Commands:" << endl;
+    cout << "  help : Print help" << endl;
+    cout << "  login DRIVER : Driver logs in" << endl;
+    cout << "  logout DRIVER : Driver logs out" << endl;
+    cout << "  order TIME INFO : Adds new order. Time format: HH:MM" << endl;
+    cout << "  serve TIME : Sends order out for delivery. Time format: HH:MM" << endl;
+    cout << "  depart TIME DRIVER : Driver departs with next order for delivery. Time format: HH:MM" << endl;
+    cout << "  deliver TIME DRIVER TIP : Driver delivers order and receives tip. Time format: HH:MM" << endl;
+    cout << "  arrive TIME DRIVER : Driver returns. Time format: HH:MM" << endl;
+    cout << "  status : Status of orders and drivers" << endl;
+    cout << "  summary : Statistics of all drivers and orders" << endl;
+    cout << "  quit : Terminates the program" << endl;
+    cout << endl;
+}
+
+void order(const Time& time, const string info, Restaurant& arestaurant) 
+{
+    Order* anOrder = new Order(time,info);
+    arestaurant.addOrder(anOrder);
+}
+
+void serve(const Time& time, Restaurant& arestaurant) 
+{
+    arestaurant.serveNextOrder();
 }
